d4ad9f599adc67c33657d92853622f526bb22626
atsjason <98031074+atsjason@users.noreply.github.com>
2024-04-18 10:15:18 -0400

    Create OfficeUpdate.bat

---
+++ OfficeUpdate.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /changesetting Channel=Current
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user displaylevel=false forceappshutdown=true
+
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /changesetting Channel=Current
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user displaylevel=false
